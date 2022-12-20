35b5bd5e27e0419fe759bcf2013fd481b9f4fa1c
Habib <habibayrilmaz@gmail.com>
2022-12-20 19:36:01 +0300

    proje tamamlandı

--- src/main/java/com/example/finalprojecthabibbackend/exception/NotFoundException.java
+++ src/main/java/com/example/finalprojecthabibbackend/exception/NotFoundException.java
@@ -4,5 +4,5 @@ public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
     }
-    //ExceptionHandler
+    //ExceptionHandle
 }
